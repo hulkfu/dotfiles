bee43b99609a6ccb1b3761b2fc5a74f8b53921bf
Hulk Foo <fudhao@gmail.com>
2017-11-24 22:27:38 +0800

    sync atom config.

---
+++ sync_atom.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+echo "Sync Atom ..."
+
+function link {
+  target_filepath="$HOME/.atom/$1" 
+  if [ -f $target_filepath ]; then
+    echo "$1 already exists and remove it."
+    rm $target_filepath
+  fi
+  ln -s $2 $target_filepath
+}
+
+# soft link config files
+for file in "./.atom"/*
+do
+  realpath=`realpath $file`
+  filename=`basename $file`
+  echo $realpath
+  link $filename $realpath
+done
+
+# install package-sync atom package
+# apm install package-sync
+
+
+
+
+
